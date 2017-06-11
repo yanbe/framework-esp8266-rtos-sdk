e34a373c601db35e40566a9495406e36422e8896
yanbe <y.yanbe@gmail.com>
2017-06-11 13:40:04 +0900

    Remove libdriver.a to enforce build driver from source

--- package.bat
+++ package.bat
@@ -2,6 +2,7 @@ curl -L https://github.com/espressif/ESP8266_RTOS_SDK/archive/master.tar.gz | ta
 curl -L https://github.com/esp8266/esp8266-wiki/raw/master/libs/libhal.a > ESP8266_RTOS_SDK-master/lib/libhal.a
 c:/Users/yanbe/.platformio/packages/toolchain-xtensa/bin/xtensa-lx106-elf-objcopy.exe -N printf -N putchar -N puts ESP8266_RTOS_SDK-master/lib/libcirom.a
 patch -p0 < driver.patch
+rm ESP8266_RTOS_SDK-master/lib/libdriver.a
 cp package.json ESP8266_RTOS_SDK-master/package.json
 tar zcf ESP8266_RTOS_SDK-master.tar.gz ESP8266_RTOS_SDK-master
 sha1sum ESP8266_RTOS_SDK-master.tar.gz
