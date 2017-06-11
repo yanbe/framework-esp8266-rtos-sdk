bea79cbe1fe282bf70d50fc85b4679da77978e86
yanbe <y.yanbe@gmail.com>
2017-06-11 13:20:27 +0900

    Applied patch for i2c_master.c to avoid undefined referernce due to original SDK packaging error

--- package.bat
+++ package.bat
@@ -1,6 +1,7 @@
 curl -L https://github.com/espressif/ESP8266_RTOS_SDK/archive/master.tar.gz | tar xz
 curl -L https://github.com/esp8266/esp8266-wiki/raw/master/libs/libhal.a > ESP8266_RTOS_SDK-master/lib/libhal.a
 c:/Users/yanbe/.platformio/packages/toolchain-xtensa/bin/xtensa-lx106-elf-objcopy.exe -N printf -N putchar -N puts ESP8266_RTOS_SDK-master/lib/libcirom.a
+patch -p0 < driver.patch
 cp package.json ESP8266_RTOS_SDK-master/package.json
 tar zcf ESP8266_RTOS_SDK-master.tar.gz ESP8266_RTOS_SDK-master
 sha1sum ESP8266_RTOS_SDK-master.tar.gz
